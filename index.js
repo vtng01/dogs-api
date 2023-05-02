5d2887059cb25c613d6fed3c4852a10322c08dda
Van Nguyen <vanthoai@fb.com>
2023-05-02 16:24:38 +0100

    wrote test for post and delete end points

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Dog } = require('./db');
-const { sequelize } = require('./db/db');
-const { Op } = require('sequelize');
-
+const { Dog } = require("./db");
+const { sequelize } = require("./db/db");
+const { Op } = require("sequelize");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/dogs', async (req, res, next) => {
+app.post("/dogs", async (req, res, next) => {
   try {
-    const {name, breed, color, description} = req.body;
-    const dog = await Dog.create({name, breed, color, description});
+    const { name, breed, color, description } = req.body;
+    const dog = await Dog.create({ name, breed, color, description });
     res.send(dog);
   } catch (error) {
     next(error);
   }
 });
 
-app.delete('/dogs/:id', async (req, res, next) => {
+app.delete("/dogs/:id", async (req, res, next) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const existingDog = await Dog.findByPk(id);
-    if(!existingDog) {
+    if (!existingDog) {
       res.status(404).send(`Dog with id ${id} not found`);
       return;
     }
-    await Dog.destroy({where: {id}});
+    await Dog.destroy({ where: { id } });
     res.send(`deleted dog with id ${id}`);
   } catch (error) {
     next(error);
   }
 });
 
-app.get('/dogs', async (req, res, next) => {
+app.get("/dogs", async (req, res, next) => {
   try {
     const where = {};
-    for(const key of ['name', 'description', 'breed', 'color']) {
-      if(req.query[key]) {
-        where[key] =  {
-          [Op.like]: `%${req.query[key]}%` // search within the string, not only exact matches
+    for (const key of ["name", "description", "breed", "color"]) {
+      if (req.query[key]) {
+        where[key] = {
+          [Op.like]: `%${req.query[key]}%`, // search within the string, not only exact matches
         };
       }
     }
-        
+
     const dogs = await Dog.findAll({
-      where
+      where,
     });
     res.send(dogs);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
--- index.test.js
+++ index.test.js
@@ -1,36 +1,66 @@
-const request = require('supertest');
+const request = require("supertest");
 // express app
-const app = require('./index');
+const app = require("./index");
 
 // db setup
-const { sequelize, Dog } = require('./db');
-const seed = require('./db/seedFn');
-const {dogs} = require('./db/seedData');
+const { sequelize, Dog } = require("./db");
+const seed = require("./db/seedFn");
+const { dogs } = require("./db/seedData");
 
-describe('Endpoints', () => {
-    // to be used in POST test
-    const testDogData = {
-        breed: 'Poodle',
-        name: 'Sasha',
-        color: 'black',
-        description: 'Sasha is a beautiful black pooodle mix.  She is a great companion for her family.'
-    };
+describe("Endpoints", () => {
+  // to be used in POST test
+  const testDogData = {
+    breed: "Poodle",
+    name: "Sasha",
+    color: "black",
+    description:
+      "Sasha is a beautiful black pooodle mix.  She is a great companion for her family.",
+  };
 
-    beforeAll(async () => {
-        // rebuild db before the test suite runs
-        await seed();
+  beforeAll(async () => {
+    // rebuild db before the test suite runs
+    await seed();
+  });
+
+  describe("GET /dogs", () => {
+    it("should return list of dogs with correct data", async () => {
+      // make a request
+      const response = await request(app).get("/dogs");
+      // assert a response code
+      expect(response.status).toBe(200);
+      // expect a response
+      expect(response.body).toBeDefined();
+      // toEqual checks deep equality in objects
+      expect(response.body[0]).toEqual(expect.objectContaining(dogs[0]));
+    });
+  });
+
+  describe("POST /dogs", () => {
+    it("should create a new dog entry with data sent", async () => {
+      const testDogData = {
+        name: "dogName",
+        breed: "testBreed",
+        color: "testColor",
+        description: "testDescription",
+      };
+      const response = await request(app).post("/dogs").send(testDogData);
+      const { name, breed, color, description } = response.body;
+      expect(response.status).toBe(200);
+      expect({ name, breed, color, description }).toEqual(testDogData);
+    });
+  });
+
+  describe("DELETE /dogs/:id", () => {
+    it("should return with not found message when id is not found", async () => {
+      const response = await request(app).delete("/dogs/-1");
+      expect(response.status).toBe(404);
+      expect(response.text).toBe("Dog with id -1 not found");
     });
 
-    describe('GET /dogs', () => {
-        it('should return list of dogs with correct data', async () => {
-            // make a request
-            const response = await request(app).get('/dogs');
-            // assert a response code
-            expect(response.status).toBe(200);
-            // expect a response
-            expect(response.body).toBeDefined();
-            // toEqual checks deep equality in objects
-            expect(response.body[0]).toEqual(expect.objectContaining(dogs[0]));
-        });
+    it("should return with success message when deletion is successful", async () => {
+      const response = await request(app).delete("/dogs/1");
+      expect(response.status).toBe(200);
+      expect(response.text).toBe("deleted dog with id 1");
     });
+  });
 });
